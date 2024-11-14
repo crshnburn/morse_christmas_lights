eaedb13eb4447a827a9a71a47bac5e0bf5ca2488
Andrew Smithson <smithson@uk.ibm.com>
2024-11-14 09:32:12 +0000

    Create morse code dictionary

---
+++ code.py
@@ -0,0 +1,32 @@
+DIT = 0.1
+DAH = 3*DIT
+LETTER_GAP = 3*DIT
+SPACE = 7*DIT
+
+MORSE = {
+    "a": [DIT, DAH],
+    "b": [DAH, DIT, DIT, DIT],
+    "c": [DAH, DIT, DAH, DIT],
+    "d": [DAH, DIT, DIT],
+    "e": [DIT],
+    "f": [DIT, DIT, DAH, DIT],
+    "g": [DAH, DAH, DIT],
+    "h": [DIT, DIT, DIT, DIT],
+    "j": [DIT, DAH, DAH, DAH],
+    "k": [DAH, DIT, DAH],
+    "l": [DIT, DAH, DIT, DIT],
+    "m": [DAH, DAH],
+    "n": [DAH, DIT],
+    "o": [DAH, DAH, DAH],
+    "p": [DIT, DAH, DAH, DIT],
+    "q": [DAH, DAH< DIT, DAH],
+    "r": [DIT, DAH, DIT],
+    "s": [DIT, DIT, DIT],
+    "t": [DAH],
+    "u": [DIT, DIT, DAH],
+    "v": [DIT, DIT, DIT, DAH],
+    "w": [DIT, DAH, DAH],
+    "x": [DAH, DIT, DIT, DAH],
+    "y": [DAH, DIT, DAH, DAH],
+    "z": [DAH, DAH, DIT, DIT],
+}
